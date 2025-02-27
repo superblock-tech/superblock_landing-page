4f8667908ae1304e0d8118a52ad25e3400bde31e
Hleb Prakhnitski <thane.crios@gmail.com>
2025-02-27 22:59:27 +0300

    improved contact form

--- Backend/app/Http/Controllers/ContactFormController.php
+++ Backend/app/Http/Controllers/ContactFormController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\ContactsExport;
+use App\Imports\ContactsImport;
 use App\Models\ContactForm;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ContactFormController extends Controller
 {
@@ -19,7 +24,7 @@ class ContactFormController extends Controller
     /**
      * Display a listing of the resource.
      */
-    
+
     public function joinUs()
     {
         $contacts = ContactForm::where('joinWhitelist', true)->get();
@@ -61,5 +66,17 @@ class ContactFormController extends Controller
         } else {
             return redirect()->route('contactForm')->with('error', 'Failed to delete contact form');
         }
+    }
+
+    final public function import(Request $request): RedirectResponse
+    {
+        Excel::import(new ContactsImport(), $request->file('file'));
+
+        return back()->with('success', 'Contacts Imported Successfully!');
+    }
+
+    final public function export(): BinaryFileResponse
+    {
+        return Excel::download(new ContactsExport(), 'contacts.xlsx');
     }
 }
