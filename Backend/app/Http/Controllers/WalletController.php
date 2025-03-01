5d9671c69dc379d112ac76ce62a8b96277fb21aa
Hleb Prakhnitski <thane.crios@gmail.com>
2025-03-01 22:59:47 +0300

    improved wallet controller

--- Backend/app/Http/Controllers/WalletController.php
+++ Backend/app/Http/Controllers/WalletController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\WalletsExport;
+use App\Imports\WalletsImport;
 use App\Models\Wallet;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class WalletController extends Controller
 {
@@ -24,6 +29,7 @@ class WalletController extends Controller
     public function getWallets()
     {
         $wallets = Wallet::all();
+
         return response()->json($wallets);
     }
 
@@ -59,5 +65,17 @@ class WalletController extends Controller
         $wallet->delete();
 
         return redirect()->route('walletAddress')->with('success', 'Wallet deleted successfully');
+    }
+
+    final public function import(Request $request): RedirectResponse
+    {
+        Excel::import(new WalletsImport(), $request->file('file'));
+
+        return back()->with('success', 'Wallets Imported Successfully!');
+    }
+
+    final public function export(): BinaryFileResponse
+    {
+        return Excel::download(new WalletsExport(), 'wallets.xlsx');
     }
 }
