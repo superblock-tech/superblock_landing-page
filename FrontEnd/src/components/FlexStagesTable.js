b6e3f053e5f96bc6978710f2a5ed6a1e9a6edb90
Hleb Prakhnitski <thane.crios@gmail.com>
2025-04-15 19:54:13 +0300

    added tokenomics near to table

--- FrontEnd/src/components/FlexStagesTable.js
+++ FrontEnd/src/components/FlexStagesTable.js
@@ -66,9 +66,20 @@ const FlexStagesTable = () => {
     const scrollRef = useHorizontalScroll();
     return (
         <section>
-            <h1 id="tokenDistribution" className="mt-10 text-[#7B36B6] text-[30px] lg:text-[55px] font-futura-600 leading-[35px] lg:leading-[64px] text-center sm:max-w-[1047px] mx-auto max-w-[303px]">
+            <h1 id="tokenDistribution"
+                className="mt-10 text-[#7B36B6] text-[30px] lg:text-[55px] font-futura-600 leading-[35px] lg:leading-[64px] text-center sm:max-w-[1047px] mx-auto max-w-[303px]">
                 $SBX Token Distribution
             </h1>
+            <p className=" text-[#686868] text-[12px] lg:text-[24px] font-futura-600 leading-[12px] lg:leading-[36px] text-center sm:max-w-[1047px] mx-auto max-w-[303px]">
+                Tokenomics:
+                The total supply of $SBX tokens is capped at 1,000,000,000. Of this, 100,000,000 $SBX tokens are
+                allocated for the presale, split evenly across 10 phases with 10 million tokens per phase. Any unsold
+                tokens from a phase will be returned to the treasury to ensure that circulating supply reflects actual
+                market demand.
+                If the soft cap is not reached, participants will receive a full refund, excluding any network
+                transaction fees. After the presale and token airdrop, the community will vote through the DAO on
+                whether to retain or permanently burn any unsold tokens
+            </p>
             <div className="p-8 flex justify-center items-center">
                 <div
                     className="bg-gradient-to-r from-purple-200 to-blue-200 p-4 sm:p-6 lg:p-10 rounded-2xl shadow-lg w-full max-w-sm md:max-w-lg lg:max-w-6xl mx-auto space-y-12 bg-white bg-opacity-20">
