501a8e9eeafe5fe3a84d106bb9686b9fc5b215c1
Hleb Prakhnitski <thane.crios@gmail.com>
2025-04-22 10:16:41 +0300

    added migration for icons

---
+++ Backend/database/migrations/2025_04_22_063027_add_symbol_field_to_cryptos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cryptos', function (Blueprint $table) {
+            $table->string('icon')->nullable();
+        });
+
+        Schema::table('crypto_networks', function (Blueprint $table) {
+            $table->string('icon')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cryptos', function (Blueprint $table) {
+            $table->dropColumn('icon');
+        });
+
+        Schema::table('crypto_networks', function (Blueprint $table) {
+            $table->dropColumn('icon');
+        });
+    }
+};
