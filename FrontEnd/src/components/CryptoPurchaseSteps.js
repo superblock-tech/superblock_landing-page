b860cdc93c349af4f51b65de549eeda56ad6dded
Hleb Prakhnitski <thane.crios@gmail.com>
2025-04-25 18:59:23 +0300

    improved steps on mobile

--- FrontEnd/src/components/CryptoPurchaseSteps.js
+++ FrontEnd/src/components/CryptoPurchaseSteps.js
@@ -59,8 +59,9 @@ export default function CryptoPurchaseSteps() {
 function Step({title, description, icons, image, reverse}) {
     return (
         <div
-            className={`flex items-center ${reverse ? 'flex-row-reverse' : ''} space-x-6 py-6 border-b border-gray-300`}>
-            <div className="flex-shrink-0 w-1/3 flex justify-center">
+            className={`flex flex-col md:flex-row ${reverse ? 'md:flex-row-reverse' : ''} items-center space-y-6 md:space-y-0 md:space-x-6 py-6 border-b border-gray-300`}>
+
+            <div className="flex-shrink-0 w-full md:w-1/3 flex justify-center">
                 {image}
                 {!image && (
                     <div className="flex space-x-4">
@@ -69,18 +70,21 @@ function Step({title, description, icons, image, reverse}) {
                     </div>
                 )}
             </div>
-            <div className="w-2/3">
+
+            <div className="w-full md:w-2/3 text-center md:text-left">
                 <h2 className="text-2xl font-bold mb-4">{title}</h2>
                 <p className="text-gray-700 mb-4">{description}</p>
             </div>
         </div>
+
     );
 }
 
 function WalletButton({name}) {
     return (
-        <button className=" rounded-[12px] bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF] hover:from-[#7B36B6] hover:to-[#1BA3FF] text-white px-4 py-2 rounded flex items-center space-x-2">
-            <img style={{width: '200px'}} src={'/assets/images/' + name +'.png'}></img>
+        <button
+            className=" rounded-[12px] bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF] hover:from-[#7B36B6] hover:to-[#1BA3FF] text-white px-4 py-2 rounded flex items-center space-x-2">
+            <img style={{width: '200px'}} src={'/assets/images/' + name + '.png'}></img>
         </button>
     );
 }
