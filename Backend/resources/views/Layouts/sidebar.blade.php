a1085472d8aa4987e79d0ab06922bb2b6e4a6784
Hleb Prakhnitski <thane.crios@gmail.com>
2025-04-30 21:06:47 +0400

    label fix

--- Backend/resources/views/Layouts/sidebar.blade.php
+++ Backend/resources/views/Layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
     <ul class="nav flex-column">
         <li class="nav-item">
             <a class="nav-link active" href="{{ route("dashboard") }}">
-                Generate Codes
+                Founders Circle
             </a>
         </li>
         <li class="nav-item ">
