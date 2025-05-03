13ec5e5b06daecb6fed3db4a7645cca2d6c0cd07
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-03 23:23:03 +0400

    created whitelist calculator

---
+++ Backend/app/Console/Commands/CalculateDataForActiveWhitelist.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\PresaleTransaction;
+use App\Models\Whitelist;
+use Illuminate\Console\Command;
+
+class CalculateDataForActiveWhitelist extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:calculate-data-for-active-whitelist';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $whitelist = Whitelist::query()->where('is_active', '=', 1)->first();
+
+        if ($whitelist) {
+            $presaleTransactions = PresaleTransaction::query()->select('wallet_address')->groupBy('wallet_address')->get();
+            $transactionsCount = count($presaleTransactions->pluck('wallet_address')->toArray());
+
+            $whitelist->usdtRaised = round(PresaleTransaction::query()->sum('usdt_amount'), 2);
+            $whitelist->sbx_allocated = round(PresaleTransaction::query()->sum('sbx_price'), 2);
+
+            $whitelist->holders = $transactionsCount;
+            $whitelist->save();
+
+        }
+    }
+}
