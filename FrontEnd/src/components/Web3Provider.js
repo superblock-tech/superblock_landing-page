449369ef68c55f644d6c920645112dedf1129694
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-14 18:38:46 +0400

    big connector fix

--- FrontEnd/src/components/Web3Provider.js
+++ FrontEnd/src/components/Web3Provider.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import {createConfig, http, WagmiProvider} from 'wagmi'
-import { mainnet, sepolia, polygon, arbitrum } from 'wagmi/chains'
-import { injected, metaMask, walletConnect } from '@wagmi/connectors'
+import React, {useEffect, useState} from 'react';
+import {createConfig, http, WagmiProvider} from 'wagmi';
+import {arbitrum, mainnet, polygon, sepolia} from "wagmi/chains";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {ConnectKitProvider} from "connectkit";
+import {ConnectKitProvider, getDefaultConfig} from 'connectkit';
 
-export const config = createConfig({
-    chains: [mainnet, sepolia, polygon, arbitrum],
-    transports: {
-        [mainnet.id]: http(),
-        [sepolia.id]: http(),
-        [polygon.id]: http(),
-        [arbitrum.id]: http(),
-    },
-    connectors: [
-        injected({ shimDisconnect: true }),
-        metaMask({ shimDisconnect: true }),
-        walletConnect({
-            projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID || '',
-            metadata: {
-                name: '$SBXToken',
-                description: '$SBX Token – Fueling DeFi, Tokenization & DAO Governance',
-                url: 'https://sbxtoken.com',
-                icons: ['https://sbxtoken.com/favicon.ico'],
-            },
-        }),
-    ],
-    autoConnect: true,
-})
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-export const Web3Provider = ({children}) => {
+export const Web3Provider = ({ children }) => {
+    const [config, setConfig] = useState(null)
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const checkEthereum = async () => {
+                if (window.ethereum) {
+                    const conf = createConfig(
+                        getDefaultConfig({
+                            chains: [mainnet, polygon, sepolia, arbitrum],
+                            [mainnet.id]: http(),
+                            walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+                            appName: '$SBXToken',
+                            autoConnect: true,
+                        })
+                    )
+                    setConfig(conf)
+                } else {
+                    console.warn('window.ethereum не найден (Trust Wallet, Metamask и т.п.)')
+                }
+            }
+
+            checkEthereum()
+        }
+    }, [])
+
+    if (!config) return null
+
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
                 <ConnectKitProvider debugMode>{children}</ConnectKitProvider>
             </QueryClientProvider>
         </WagmiProvider>
-    );
-};
+    )
+}
