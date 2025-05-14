6e9d886ad2af1088920d50c414efd7d62df27855
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-14 18:03:00 +0400

    big connector fix

--- FrontEnd/src/components/Web3Provider.js
+++ FrontEnd/src/components/Web3Provider.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import {createConfig, http, WagmiProvider} from 'wagmi';
-import {arbitrum, mainnet, polygon, sepolia} from "wagmi/chains";
+import {createConfig, http, WagmiProvider} from 'wagmi'
+import { mainnet, sepolia, polygon, arbitrum } from 'wagmi/chains'
+import { injected, metaMask, walletConnect } from '@wagmi/connectors'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {ConnectKitProvider, getDefaultConfig} from 'connectkit';
+import {ConnectKitProvider} from "connectkit";
 
-const config = createConfig(
-    getDefaultConfig({
-        appName: 'SuperBlock',
-        chains: [mainnet, sepolia, polygon, arbitrum],
+export const config = createConfig({
+    chains: [mainnet, sepolia, polygon, arbitrum],
+    transports: {
         [mainnet.id]: http(),
-        walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
-        autoConnect: true,
-    })
-);
+        [sepolia.id]: http(),
+        [polygon.id]: http(),
+        [arbitrum.id]: http(),
+    },
+    connectors: [
+        injected({ shimDisconnect: true }),
+        metaMask({ shimDisconnect: true }),
+        walletConnect({
+            projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID || '',
+            metadata: {
+                name: '$SBXToken',
+                description: '$SBX Token – Fueling DeFi, Tokenization & DAO Governance',
+                url: 'https://sbxtoken.com',
+                icons: ['https://sbxtoken.com/favicon.ico'],
+            },
+        }),
+    ],
+    autoConnect: true,
+})
 
 const queryClient = new QueryClient();
 
