30631ef87dd0194d8a7d756d92177aee25c35dbe
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-14 17:37:48 +0400

    connector fix

--- FrontEnd/src/components/Web3Provider.js
+++ FrontEnd/src/components/Web3Provider.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { WagmiProvider, createConfig,http } from 'wagmi';
-import {mainnet, sepolia, polygon, arbitrum} from "wagmi/chains";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import {createConfig, http, WagmiProvider} from 'wagmi';
+import {arbitrum, mainnet, polygon, sepolia} from "wagmi/chains";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ConnectKitProvider, getDefaultConfig} from 'connectkit';
 
 const config = createConfig(
-  getDefaultConfig({
-    appName: 'SuperBlock',
-    chains: [mainnet, sepolia, polygon, arbitrum],
-    [mainnet.id]: http(),
-    walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
-  })
+    getDefaultConfig({
+        appName: 'SuperBlock',
+        chains: [mainnet, sepolia, polygon, arbitrum],
+        [mainnet.id]: http(),
+        walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+        autoConnect: true,
+    })
 );
 
 const queryClient = new QueryClient();
 
-export const Web3Provider = ({ children }) => {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider debugMode>{children}</ConnectKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+export const Web3Provider = ({children}) => {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <ConnectKitProvider debugMode>{children}</ConnectKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
 };
