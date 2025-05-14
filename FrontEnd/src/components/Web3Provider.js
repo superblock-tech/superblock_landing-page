7decdf057db1dc30f85d7fbd5d8b758b06de1efe
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-14 19:02:59 +0400

    fix

--- FrontEnd/src/components/Web3Provider.js
+++ FrontEnd/src/components/Web3Provider.js
@@ -1,44 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {createConfig, http, WagmiProvider} from 'wagmi';
 import {arbitrum, mainnet, polygon, sepolia} from "wagmi/chains";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ConnectKitProvider, getDefaultConfig} from 'connectkit';
 
+const config = createConfig(
+    getDefaultConfig({
+        appName: '$SBXToken',
+        chains: [mainnet, sepolia, polygon, arbitrum],
+        [mainnet.id]: http(),
+        walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+        autoConnect: true,
+    })
+);
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-export const Web3Provider = ({ children }) => {
-    const [config, setConfig] = useState(null)
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const checkEthereum = async () => {
-                if (window.ethereum) {
-                    const conf = createConfig(
-                        getDefaultConfig({
-                            chains: [mainnet, polygon, sepolia, arbitrum],
-                            [mainnet.id]: http(),
-                            walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
-                            appName: '$SBXToken',
-                            autoConnect: true,
-                        })
-                    )
-                    setConfig(conf)
-                } else {
-                    console.warn('window.ethereum не найден (Trust Wallet, Metamask и т.п.)')
-                }
-            }
-
-            checkEthereum()
-        }
-    }, [])
-
-    if (!config) return null
-
+export const Web3Provider = ({children}) => {
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
                 <ConnectKitProvider debugMode>{children}</ConnectKitProvider>
             </QueryClientProvider>
         </WagmiProvider>
-    )
-}
+    );
+};
