d7ef367c80f86427c2cd097c85c80c87e1f11901
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-16 19:02:24 +0400

    improved app js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,40 +1,48 @@
-import { Toaster } from "react-hot-toast";
-import { AuthProvider } from "./contexts/AuthContext";
-import { Route, Routes } from "react-router-dom";
+import {Toaster} from "react-hot-toast";
+import {AuthProvider} from "./contexts/AuthContext";
+import {Route, Routes, useLocation} from "react-router-dom";
 import HomePage from "./pages";
-import PrivateRoute from "./components/PrivateRoute";
 import ProfilePage from "./pages/profile";
 import Header from "./components/Header";
-import { PresaleContextProvider } from "./contexts/PresaleContext";
-import { Web3Provider } from "./components/Web3Provider";
+import {PresaleContextProvider} from "./contexts/PresaleContext";
+import {Web3Provider} from "./components/Web3Provider";
+import {useEffect} from "react";
+import {pageview} from "./utils/gtag";
 
 function App() {
-  return (
-    <AuthProvider>
-        <Web3Provider>
-          <PresaleContextProvider>
-            <Toaster />
-            <Header />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route
-                path="/profile"
-                element={
-                    <ProfilePage />
-                }
-              />
-                <Route
-                    path="/presale"
-                    element={
-                        <ProfilePage />
-                    }
-                />
+    const location = useLocation();
 
-            </Routes>
-          </PresaleContextProvider>
-          </Web3Provider>
-    </AuthProvider>
-  );
+    useEffect(() => {
+        pageview(location.pathname + location.search);
+    }, [location]);
+
+
+    return (
+        <AuthProvider>
+            <Web3Provider>
+                <PresaleContextProvider>
+                    <Toaster/>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route
+                            path="/profile"
+                            element={
+                                <ProfilePage/>
+                            }
+                        />
+                        <Route
+                            path="/presale"
+                            element={
+                                <ProfilePage/>
+                            }
+                        />
+
+                    </Routes>
+                </PresaleContextProvider>
+            </Web3Provider>
+        </AuthProvider>
+    );
 }
 
 export default App;
