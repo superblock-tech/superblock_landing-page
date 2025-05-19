c3c76bd65e6694ba96fa113a8c0d76899412e89b
Hleb Prakhnitski <thane.crios@gmail.com>
2025-05-19 05:50:47 +0400

    added new params for smart contract generation

--- FrontEnd/src/components/SendFromConnectKitButton.js
+++ FrontEnd/src/components/SendFromConnectKitButton.js
@@ -15,26 +15,31 @@ const SendEthButton = ({amount, sbxAmount, selectedToken, selectedNetwork}) => {
     const TOKENS = {
         MATIC: {
             USDC: {
-                address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+                address: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
                 decimals: 6,
-                chain: polygon
+                chain: polygon,
+                function: 'extendedTransaction'
+
             },
             USDT: {
                 address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
                 decimals: 6,
-                chain: polygon
+                chain: polygon,
+                function: 'contract'
             },
         },
         ERC20: {
             USDC: {
                 address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
                 decimals: 6,
-                chain: mainnet
+                chain: mainnet,
+                function: 'extendedTransaction'
             },
             USDT: {
                 address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
                 decimals: 6,
-                chain: mainnet
+                chain: mainnet,
+                function: 'contract'
             },
 
         },
