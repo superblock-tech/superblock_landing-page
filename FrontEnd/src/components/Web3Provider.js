5a965645d4e1a9bd4d00477c932ee01e5ccc152f
Hleb Prakhnitski <thane.crios@gmail.com>
2025-07-07 00:49:24 +0400

    fixed web3 provider

--- FrontEnd/src/components/Web3Provider.js
+++ FrontEnd/src/components/Web3Provider.js
@@ -3,29 +3,31 @@ import {createConfig, http, WagmiProvider} from 'wagmi';
 import {arbitrum, mainnet, polygon, sepolia} from "wagmi/chains";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ConnectKitProvider} from 'connectkit';
-import {injected} from '@wagmi/connectors'
+import {injected, walletConnect} from '@wagmi/connectors'
 
 
-const config = createConfig(
-    {
-        appName: '$SBXToken',
-        chains: [mainnet, polygon, sepolia, arbitrum],
-        transports: {
-            [mainnet.id]: http(),
-            [polygon.id]: http(),
-            [sepolia.id]: http(),
-            [arbitrum.id]: http(),
-        },
-        projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
-        connectors: [
-            injected({
-                shimDisconnect: true
-            }),
-        ],
-        ssr: false,
-        autoConnect: true,
-    }
-);
+const config = createConfig({
+    chains: [mainnet, polygon, sepolia, arbitrum],
+    connectors: [
+        injected(),
+        walletConnect({
+            projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+            showQrModal: false,
+            metadata: {
+                name: '$SBXToken',
+                description: 'SBX Token Application',
+                url: window.location.origin,
+                icons: [`${window.location.origin}/favicon.ico`]
+            },
+        }),
+    ],
+    transports: {
+        [mainnet.id]: http(),
+        [polygon.id]: http(),
+        [sepolia.id]: http(),
+        [arbitrum.id]: http(),
+    },
+})
 
 const queryClient = new QueryClient();
 
