e2238d9355e7be7baedd6f6a8e1881493a18ed0f
Hleb Prakhnitski <thane.crios@gmail.com>
2025-07-08 16:37:41 +0400

    fix

--- FrontEnd/src/components/Web3Provider.js
+++ FrontEnd/src/components/Web3Provider.js
@@ -9,7 +9,7 @@ import {injected, walletConnect} from '@wagmi/connectors'
 const config = createConfig({
     chains: [mainnet, polygon, sepolia, arbitrum],
     connectors: [
-        injected(),
+        // injected(),
         walletConnect({
             projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
             metadata: {
